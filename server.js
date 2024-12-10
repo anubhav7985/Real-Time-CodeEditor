83d4813be20044a2b55682543622ab5df5b023e6
anubhav7985 <anubhav.sri172@gmail.com>
2024-12-10 18:37:00 +0530

    third commit

--- server.js
+++ server.js
@@ -2,24 +2,28 @@ import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
 import Actions from './Actions.js'
-import path from 'path'
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
 app.use(express.static('dist'))
-app.use((req,res,next) => {
-    res.sendFile(path.join(__dirname,'dist','index.html'))
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 
-const userSocketMap = {} 
+const userSocketMap = {}
 function getAllConnectedClients(roomId) {
     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
         return {
             socketId,
-            username:userSocketMap[socketId]
+            username: userSocketMap[socketId]
         }
     })
 }
@@ -31,7 +35,7 @@ io.on('connection', (socket) => {
         userSocketMap[socket.id] = username
         socket.join(roomId);
         const clients = getAllConnectedClients(roomId)
-        clients.forEach(({socketId}) => {
+        clients.forEach(({ socketId }) => {
             io.to(socketId).emit(Actions.JOINED, {
                 clients,
                 username,
@@ -40,12 +44,12 @@ io.on('connection', (socket) => {
         })
     })
 
-    socket.on(Actions.CODE_CHANGE,({roomId,code}) => {
-        socket.in(roomId).emit(Actions.CODE_CHANGE, {code});
+    socket.on(Actions.CODE_CHANGE, ({ roomId, code }) => {
+        socket.in(roomId).emit(Actions.CODE_CHANGE, { code });
     })
-    
-    socket.on(Actions.SYNC_CODE,({socketId,code}) => {
-        io.to(socketId).emit(Actions.CODE_CHANGE, {code});
+
+    socket.on(Actions.SYNC_CODE, ({ socketId, code }) => {
+        io.to(socketId).emit(Actions.CODE_CHANGE, { code });
     })
 
     socket.on('disconnecting', () => {
