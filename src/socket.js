58ccfa5d590fd322faecf453594230d7e0fd132b
anubhav7985 <anubhav.sri172@gmail.com>
2024-12-11 17:48:26 +0530

    seven commit

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 const env = import.meta.env
 const node_env = env.NODE_ENV || 'development';
-const backendUrl = 'http://localhost:5000'
+const backendUrl = node_env.REACT_APP_BACKEND_URL
 
 export const initSocket = async () => {
     console.log(backendUrl);
